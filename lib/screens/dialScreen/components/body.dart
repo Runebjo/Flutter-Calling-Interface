d3d31a66a393818f1eccea90399c8e73e6dfafb1
Rune Bjørsnøs <rune.bjorsnos@gmail.com>
2020-11-10 22:52:59 +0100

    ADD dial buttons

--- lib/screens/dialScreen/components/body.dart
+++ lib/screens/dialScreen/components/body.dart
@@ -1,29 +1,71 @@
 import 'package:calling/components/dial_user_pic.dart';
 import 'package:calling/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'dial_button.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Text(
-            "Anna Williams",
-            style: Theme.of(context)
-                .textTheme
-                .headline4
-                .copyWith(color: Colors.white),
-          ),
-          Text(
-            "Calling...",
-            style: TextStyle(color: Colors.white60),
-          ),
-          VerticalSpacing(),
-          DialUserPic(
-            image: "assets/images/calling_face.png",
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              "Anna Williams",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: Colors.white),
+            ),
+            Text(
+              "Calling...",
+              style: TextStyle(color: Colors.white60),
+            ),
+            VerticalSpacing(),
+            DialUserPic(
+              image: "assets/images/calling_face.png",
+            ),
+            Spacer(),
+            Wrap(
+              alignment: WrapAlignment.spaceBetween,
+              children: [
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon Mic.svg",
+                  text: "Audio",
+                ),
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon Volume.svg",
+                  text: "Microphone",
+                ),
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon Video.svg",
+                  text: "Video",
+                ),
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon Message.svg",
+                  text: "Message",
+                ),
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon User.svg",
+                  text: "Add Contact",
+                ),
+                DialButton(
+                  press: () {},
+                  iconSrc: "assets/icons/Icon Voicemail.svg",
+                  text: "Voice Mail",
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
